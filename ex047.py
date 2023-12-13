807e0e28263e9d05ec3780d902cf8a7d0003dd7a
Mikaelle Natsuki Yamanaka <139868907+MikaelleNatsuki@users.noreply.github.com>
2023-12-13 07:25:19 -0300

    Add files via upload

---
+++ ex047.py
@@ -0,0 +1,15 @@
+print('\33[31mEmprestimo bancário para financimento de um casa\33')
+# Lembrando que o valor da mensalidade nao pode passar de 30% do salario do usuario.
+nome = str(input('\033[37mOlá, qual o seu nome? :\033'))
+valcasa = float(input('Digite o valor da casa: '))
+anos = int(input('Em quantos anos você pretende pagar?: '))
+salario = float(input('Qual é o seu salário?: '))
+prestacao = (anos * 12)
+mensa = (valcasa / prestacao)
+if mensa > (salario * 30)/100:
+    print('Infelizmente não podemos efetuar o emprestimo, poís o valor da mensalidade é de {:.2f} que é acima de 30% do seu salário'.format(mensa))
+elif mensa <= (salario * 30)/100:
+    print('''O valor da mensalidade será de: R${:.2f}.
+Que será pago em: {} anos.
+\033[32mParábens, {} por comprar sua casa!!!
+'''.format(mensa, anos, nome))
