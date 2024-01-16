2da17b40673d7c48b60d9accc52343b34caac34d
Mikaelle Natsuki Yamanaka <139868907+MikaelleNatsuki@users.noreply.github.com>
2024-01-15 22:58:09 -0300

    ex015Precodeumproduto.py

--- ex015.py
+++
@@ -1,5 +0,0 @@
-print ('Preço do produto.')
-vp = float(input('Digite o valor total da compra dos produtos:R$ '))
-desc = vp * 0.05
-vf = vp * 0.95
-print ('O valor total da compra é de {} \n 0 valor do desconto de 5% da compra é: {}R$ \n O valor total é de: {} '.format(vp,desc,vf))
