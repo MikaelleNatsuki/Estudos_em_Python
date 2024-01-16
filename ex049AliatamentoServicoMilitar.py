bd4619ba3fe52b587279eff0f82767a6fbc57e26
Mikaelle Natsuki Yamanaka <139868907+MikaelleNatsuki@users.noreply.github.com>
2024-01-16 07:52:22 -0300

    ex049AliatamentoServicoMilitar.py

--- ex049AliatamentoServicoMilitar.py
+++ ex049AliatamentoServicoMilitar.py
@@ -1,6 +1,6 @@
 from datetime import date
-print('\033[34m-\033'*32)
-print('\033[34mAlistamento ao serviço militar\033')
+print('\033[34m-\033[m'*32)
+print('\033[34mAlistamento ao serviço militar\033[m')
 print('-'*32)
 se = str(input('Você é do sexo (Masculino/Feminino?:) ')).capitalize()
 anonasc = int(input('Qual o seu ano de nascimento?: '))
@@ -12,10 +12,10 @@ if se == 'Feminino'.capitalize():
     print('Infelizmente pessoas do sexo feminino não podem se alistar.')
 elif idade <= 17:
     print('''\033[90mSua idade atual é de : {} anos.
-Faltam {} anos para o seu alistamento.\033'''.format(idade, fal))
+Faltam {} anos para o seu alistamento.\033[m'''.format(idade, fal))
 elif idade == 17:
     print('''\033[34mSua idade atual é de : {} anos.
-Está na hora de se alistar!\033'''.format(idade))
+Está na hora de se alistar!\033[m'''.format(idade))
 else:
     print('''\033[31mSua idade atual é de : {} anos.'
-Já passou {} anos do ano em que voce deveria se alistar\033'''.format(idade, sob))
+Já passou {} anos do ano em que voce deveria se alistar\033[m'''.format(idade, sob))
